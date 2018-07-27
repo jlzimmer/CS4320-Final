377baad1566d17dea5220f47b0558bc042e3e410
Zach Watson <35540447+ZachWatsonMiz@users.noreply.github.com>
2018-07-27 00:24:34 -0400

    Created generated page to display palette

---
+++ utilities/generated.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Generated Palette</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">  
+    <link rel="stylesheet" type="text/css" href="palette.css">
+</head>
+<body>
+
+        <?php
+        require 'CSSgen.php';
+
+        echo $uno;
+        echo $dos;
+        echo $tres;
+
+        ?>
+
+        <div class="primary">
+        <div class="card-body">
+                Test
+        </div>
+        </div>
+
+        <div class="secondary">
+        <div class="card-body">
+                test
+        </div>
+        </div>
+
+        <div class="tertiary">
+        <div class="card-body">
+                test
+        </div>
+        </div>
+
+
+       
+        
+</body>
+</html>

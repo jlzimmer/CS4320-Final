a47dcf14264be20d6465b12d4fb15833cd6bab26
skylerGunn <31551615+skylerGunn@users.noreply.github.com>
2018-07-27 16:53:39 -0500

    added bootstrap link

--- generated.php
+++ generated.php
@@ -6,6 +6,7 @@
     <title>Generated Palette</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">  
     <link rel="stylesheet" type="text/css" href="palette.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
 
